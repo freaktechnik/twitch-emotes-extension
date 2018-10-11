d8f8f47a4b7972639ba62e09546b23a661b37857
Martin Giger <martin@humanoids.be>
2018-10-11 18:16:19 +0200

    feat: show emotes in config panel and expand ffz and bttv by default when there are no sub emotes

--- public/config.js
+++ public/config.js
@@ -1,94 +1,172 @@
-var twitch = window.Twitch.ext;
-var CONFIG_VERSION = "1";
-var DEFAULT_FALSE = [
-    'bttv_expanded',
-    'ffz_expanded'
-];
-twitch.onContext(function(context) {
-    document.body.classList = context.theme;
-});
-twitch.onAuthorized(function(auth) {
-    //Don't care about this currently.
-});
-//TODO somehow default to expanding FFZ and BTTV when no config was saved yet and there are nos ub emotes.
-twitch.configuration.onChanged(function() {
-    if(twitch.configuration.broadcaster && twitch.configuration.broadcaster.version == CONFIG_VERSION) {
-        var data = JSON.parse(twitch.configuration.broadcaster.content);
-        var keys = Object.keys(data);
-        var id, input;
-        var isDefault = true;
-        for(var i = 0; i < keys.length; ++i) {
-            id = keys[i];
-            input = document.getElementById(id);
-            if(!input) {
-                continue;
-            }
-            if(input.type == 'checkbox') {
-                input.checked = data[id];
-                if(isDefault && input.checked != (DEFAULT_FALSE.indexOf(input.id) === -1)) {
-                    isDefault = false;
-                }
-            }
-            else {
-                input.value = data[id];
-                if(isDefault && input.value) {
-                    isDefault = false;
-                }
-            }
+(function() {
+    "use strict";
+    var twitch = window.Twitch.ext;
+    var CONFIG_VERSION = "1";
+    var DEFAULT_FALSE = [
+        'bttv_expanded',
+        'ffz_expanded'
+    ];
+    var hasSubEmotes = false;
+    var received = {
+        config: false,
+        auth: false,
+        ready() {
+            return this.config && this.auth;
         }
-        document.getElementById("reset").disabled = isDefault;
+    };
+    function addEmotes(sectionId, emotes, tooltipExtra) {
+        tooltipExtra = tooltipExtra || '';
+        var section = document.getElementById(sectionId + "-emotes");
+        for(var i = 0; i < emotes.length; ++i) {
+            var emote = emotes[i];
+            var image = new Image(emote.width, emote.height);
+            image.src = emote.url;
+            image.alt = emote.name;
+            image.title = emote.name + tooltipExtra;
+            if(emote.hasOwnProperty("srcset")) {
+                image.srcset = emote.srcset;
+            }
+            var figure = document.createElement("figure");
+            figure.appendChild(image);
+            var item = document.createElement("li");
+            item.appendChild(figure);
+            section.appendChild(item);
+        }
     }
-});
-var saveCallTimeout;
-var saveTimeout = 1000;
-function updateConfiguration() {
-    if(saveCallTimeout) {
-        clearTimeout(saveCallTimeout);
+    function updateDefaults() {
+        if(!hasSubEmotes && received.ready() && !twitch.configuration.broadcaster) {
+            twitch.rig.log("Set initial default config without sub emotes");
+            for(var k = 0; k < DEFAULT_FALSE.length; ++k) {
+                document.getElementById(DEFAULT_FALSE[k]).checked = true;
+            }
+            DEFAULT_FALSE.length = 0;
+            updateConfiguration();
+        }
     }
-    saveCallTimeout = setTimeout(function() {
+    twitch.onContext(function(context) {
+        document.body.classList = context.theme;
+    });
+    twitch.onAuthorized(function(auth) {
+        // Race condition
+        if(!window.EmotesModel.channelId) {
+          window.EmotesModel.setAuth(auth);
+        }
+        window.EmotesModel.getChannelInfo().then(function() {
+            var gracefulFail = function() { return []; };
+            return Promise.all([
+                window.EmotesModel.getEmotes().catch(gracefulFail),
+                window.EmotesModel.getBTTVEmotes().catch(gracefulFail),
+                window.EmotesModel.getFFZEmotes().catch(gracefulFail)
+            ]);
+        }).then(function(emotes) {
+            var sectionIds = [
+                'sub',
+                'bttv',
+                'ffz'
+            ];
+            received.auth = true;
+            for(var i = 0; i < emotes.length; ++i) {
+                if(sectionIds[i] == 'sub') {
+                    for(var j = 0; j < emotes[i].length; ++j) {
+                        addEmotes(sectionIds[i], emotes[i][j].emotes, " (" + emotes[i][j].type + ")");
+                        if(!hasSubEmotes && emotes[i][j].emotes.length) {
+                            hasSubEmotes = true;
+                        }
+                    }
+                }
+                else {
+                    addEmotes(sectionIds[i], emotes[i]);
+                }
+            }
+            updateDefaults();
+        }).catch(function(e) {
+            twitch.rig.log(e.message);
+        });
+    });
+    twitch.configuration.onChanged(function() {
+        received.config = true;
+        if(twitch.configuration.broadcaster && twitch.configuration.broadcaster.version == CONFIG_VERSION) {
+            var data = JSON.parse(twitch.configuration.broadcaster.content);
+            var keys = Object.keys(data);
+            var id, input;
+            var isDefault = true;
+            for(var i = 0; i < keys.length; ++i) {
+                id = keys[i];
+                input = document.getElementById(id);
+                if(!input) {
+                    continue;
+                }
+                if(input.type == 'checkbox') {
+                    input.checked = data[id];
+                    if(isDefault && input.checked != (DEFAULT_FALSE.indexOf(input.id) === -1)) {
+                        isDefault = false;
+                    }
+                }
+                else {
+                    input.value = data[id];
+                    if(isDefault && input.value) {
+                        isDefault = false;
+                    }
+                }
+            }
+            document.getElementById("reset").disabled = isDefault;
+        }
+        else {
+            updateDefaults();
+        }
+    });
+    var saveCallTimeout;
+    var saveTimeout = 1000;
+    function updateConfiguration() {
+        //TODO add save indicator so stuff doesn't get lost.
+        if(saveCallTimeout) {
+            clearTimeout(saveCallTimeout);
+        }
+        saveCallTimeout = setTimeout(function() {
+            var inputs = document.getElementsByTagName('input');
+            var data = {};
+            var isDefault = true;
+            for(var i = 0; i < inputs.length; ++i) {
+                if(inputs[i].type == 'checkbox') {
+                    data[inputs[i].id] = inputs[i].checked;
+                    if(isDefault && inputs[i].checked != (DEFAULT_FALSE.indexOf(inputs[i].id) === -1)) {
+                        isDefault = false;
+                    }
+                }
+                else {
+                    data[inputs[i].id] = inputs[i].value;
+                    if(isDefault && inputs[i].value) {
+                        isDefault = false;
+                    }
+                }
+            }
+            twitch.configuration.set('broadcaster', CONFIG_VERSION, JSON.stringify(data));
+            document.getElementById("reset").disabled = isDefault;
+            saveCallTimeout = undefined;
+        }, saveTimeout);
+    }
+    function reset() {
         var inputs = document.getElementsByTagName('input');
-        var data = {};
-        var isDefault = true;
         for(var i = 0; i < inputs.length; ++i) {
             if(inputs[i].type == 'checkbox') {
-                data[inputs[i].id] = inputs[i].checked;
-                if(isDefault && inputs[i].checked != (DEFAULT_FALSE.indexOf(inputs[i].id) === -1)) {
-                    isDefault = false;
-                }
+                inputs[i].checked = DEFAULT_FALSE.indexOf(inputs[i].id) === -1;
             }
             else {
-                data[inputs[i].id] = inputs[i].value;
-                if(isDefault && inputs[i].value) {
-                    isDefault = false;
-                }
+                inputs[i].value = '';
             }
         }
-        twitch.configuration.set('broadcaster', CONFIG_VERSION, JSON.stringify(data));
-        document.getElementById("reset").disabled = isDefault;
-        saveCallTimeout = undefined;
-    }, saveTimeout);
-}
-function reset() {
-    var inputs = document.getElementsByTagName('input');
-    for(var i = 0; i < inputs.length; ++i) {
-        if(inputs[i].type == 'checkbox') {
-            inputs[i].checked = DEFAULT_FALSE.indexOf(inputs[i].id) === -1;
-        }
-        else {
-            inputs[i].value = '';
-        }
+        updateConfiguration();
     }
-    updateConfiguration();
-}
-document.addEventListener("DOMContentLoaded", function() {
-    var inputs = document.getElementsByTagName('input');
-    for(var i = 0; i < inputs.length; ++i) {
-        if(inputs[i].type == 'checkbox') {
-            inputs[i].addEventListener("change", updateConfiguration, false);
+    document.addEventListener("DOMContentLoaded", function() {
+        var inputs = document.getElementsByTagName('input');
+        for(var i = 0; i < inputs.length; ++i) {
+            if(inputs[i].type == 'checkbox') {
+                inputs[i].addEventListener("change", updateConfiguration, false);
+            }
+            else {
+                inputs[i].addEventListener("input", updateConfiguration, false);
+            }
         }
-        else {
-            inputs[i].addEventListener("input", updateConfiguration, false);
-        }
-    }
-    document.getElementById('reset').addEventListener("click", reset);
-}, false);
+        document.getElementById('reset').addEventListener("click", reset);
+    }, false);
+})();
--- public/viewer.js
+++ public/viewer.js
@@ -13,18 +13,6 @@
             2: "_2000",
             3: "_3000"
         },
-        SIZES: [
-            1,
-            1.5,
-            2,
-            3,
-            4
-        ],
-        BTTV_SIZES: [
-           1,
-           2,
-           3
-        ],
         config: {
             bttv_expanded: false,
             bttv_title: '',
@@ -47,19 +35,14 @@
         },
         loaded: false,
         loading: false,
-        receivedData: {
-            auth: false,
-            config: false
-        },
+        receivedConfig: false,
         visibleCredits: [],
         setAuth: function(auth) {
-            this.channelId = auth.channelId;
-            this.clientId = auth.clientId;
-            this.receivedData.auth = true;
-
-            twitch.rig.log('got auth');
-
-            if(!this.loading && !this.loaded && this.receivedData.config) {
+            // Race condition
+            if(!window.EmotesModel.channelId) {
+              window.EmotesModel.setAuth(auth);
+            }
+            if(!this.loading && !this.loaded && this.receivedConfig) {
                 this.updatePanel().catch(function(e) {
                     twitch.rig.log(e.message);
                 });
@@ -67,9 +50,9 @@
         },
         setConfig: function(config) {
             this.config = config;
-            twitch.rig.log('got config');
-            this.receivedData.config = true;
-            if(!this.loading && this.receivedData.auth) {
+
+            this.receivedConfig = true;
+            if(!this.loading && window.EmotesModel.channelId) {
                 this.updatePanel().catch(function(e) {
                     twitch.rig.log(e.message);
                 });
@@ -91,14 +74,6 @@
             document.getElementById('credits').className = 'hidden';
         },
 
-        isPopout: function() {
-            return location.search.includes("popout=true");
-        },
-
-        isProd: function() {
-            return location.search.includes("state=released");
-        },
-
         getExpandedPref: function(type) {
             switch(type) {
                 case this.TYPE.BTTV:
@@ -109,7 +84,8 @@
         },
 
         needsChannelInfo: function() {
-            return (!this.username && this.config.bttv_visible) || (this.canHaveEmotes === undefined && this.config.sub_visible);
+            return (!window.EmotesModel.username && this.config.bttv_visible) ||
+                ((window.EmotesModel.canHaveEmotes === undefined || !window.EmotesModel.username) && this.config.sub_visible);
         },
 
         updatePanel: function() {
@@ -123,14 +99,14 @@
             var promise = Promise.resolve();
             if(this.needsChannelInfo()) {
                 twitch.rig.log("needs channel info");
-                promise = this.getChannelInfo();
+                promise = window.EmotesModel.getChannelInfo();
             }
             return promise.then(function() {
                 var gracefulFail = function() { return []; };
                 return Promise.all([
-                    EmotesPanel.config.sub_visible ? EmotesPanel.getEmotes().catch(gracefulFail) : Promise.resolve([]),
-                    EmotesPanel.config.bttv_visible ? EmotesPanel.getBTTVEmotes().catch(gracefulFail) : Promise.resolve([]),
-                    EmotesPanel.config.ffz_visible ? EmotesPanel.getFFZEmotes().catch(gracefulFail) : Promise.resolve([])
+                    EmotesPanel.config.sub_visible ? window.EmotesModel.getEmotes().catch(gracefulFail) : Promise.resolve([]),
+                    EmotesPanel.config.bttv_visible ? window.EmotesModel.getBTTVEmotes().catch(gracefulFail) : Promise.resolve([]),
+                    EmotesPanel.config.ffz_visible ? window.EmotesModel.getFFZEmotes().catch(gracefulFail) : Promise.resolve([])
                 ]);
             }).then(function(emoteSets) {
                 EmotesPanel.loaded = true;
@@ -143,7 +119,7 @@
                 var base = document.getElementById("emotes");
                 var addedSomeEmotes = false;
                 var hasTwitchEmotes = false;
-                var isPopout = EmotesPanel.isPopout();
+                var isPopout = window.EmotesModel.isPopout();
                 for(var i = 0; i < emoteSets.length; ++i) {
                     if(emoteSets[i].length) {
                         if(typeMap[i] === EmotesPanel.TYPE.TWITCH && EmotesPanel.config.sub_visible) {
@@ -159,7 +135,7 @@
                             }
                         }
                         else {
-                            var expandSection = (EmotesPanel.config.popout_expand && isPopout) || EmotesPanel.getExpandedPref(typeMap[i]);
+                            var expandSection = (EmotesPanel.config.popout_expand && isPopout) || EmotesPanel.getExpandedPref(typeMap[i]) || (!twitch.configuration.broadcaster && !window.EmotesModel.canHaveEmotes);
                             var section = EmotesPanel.makeEmoteSection(typeMap[i], emoteSets[i], expandSection);
                             base.appendChild(section);
                             if(!addedSomeEmotes) {
@@ -175,7 +151,10 @@
                 else {
                     EmotesPanel.doneLoading();
                 }
-            }).catch(twitch.rig.log);
+            }).catch(function(e) {
+                EmotesPanel.noEmotes();
+                twitch.rig.log(e.message);
+            });
         },
 
         getTierFromPrice: function(price) {
@@ -201,7 +180,7 @@
                 //TODO don't show for plans user is already subbed for.
                 var link = document.createElement("a");
                 link.target = '_blank';
-                link.href = 'https://www.twitch.tv/products/' + this.username + this.TIERS[tier];
+                link.href = 'https://www.twitch.tv/products/' + EmotesModel.username + this.TIERS[tier];
                 link.title = (this.config.sub_tooltip || "Subscribe for") + " " + price;
                 link.textContent = this.config.sub_action || "Get";
                 heading.appendChild(link);
@@ -303,132 +282,6 @@
                 }
             }
             this.visibleCredits.push(saveType);
-        },
-
-        getChannelInfo: function() {
-            return fetch('https://api.twitch.tv/helix/users?id=' + this.channelId, {
-                headers: {
-                    "Client-ID": EmotesPanel.clientId
-                }
-            }).then(function(res) {
-                if(res.ok && res.status === 200) {
-                    return res.json();
-                }
-                throw new Error("could not load user status");
-            }).then(function(user) {
-                if(user.data.length) {
-                    EmotesPanel.canHaveEmotes = !!user.data[0].broadcaster_type.length;
-                    EmotesPanel.username = user.data[0].login;
-                    if(!EmotesPanel.isProd() && EmotesPanel.channelId == '24261394') {
-                        EmotesPanel.canHaveEmotes = true;
-                    }
-                }
-                else {
-                    throw new Error("no user returned");
-                }
-            }).catch(function(e) {
-                EmotesPanel.noEmotes();
-                throw e;
-            });
-        },
-
-        makeEmoteUrl: function(emoteId, size) {
-            size = size || "1.0";
-            return 'https://static-cdn.jtvnw.net/emoticons/v1/' + emoteId + '/' + size;
-        },
-
-        makeSrcSet: function(emoteId) {
-            return this.SIZES.map(function(density) {
-                return EmotesPanel.makeEmoteUrl(emoteId, density.toFixed(1)) + ' ' + density.toString(10) + 'x';
-            }).join(', ');
-        },
-
-        getEmotes: function() {
-            if(!this.canHaveEmotes) {
-                return Promise.reject("User can not have twitch emotes");
-            }
-            //TODO use official Twitch API if there ever is one (hint, hint)
-            return fetch('https://te.humanoids.be/emotesets/' + this.channelId + '.json').then(function(res) {
-                if(res.ok && res.status === 200) {
-                    return res.json();
-                }
-                throw new Error("Could not get sub tier details");
-            }).then(function(json) {
-                return fetch('https://api.twitch.tv/kraken/chat/emoticon_images?emotesets=' + Object.values(json).join(','), {
-                    headers: {
-                        "Client-ID": EmotesPanel.clientId,
-                        Accept: "application/vnd.twitchtv.v5+json"
-                    }
-                }).then(function(r) {
-                    if(r.ok && r.status === 200) {
-                        return Promise.all([
-                            r.json(),
-                            json
-                        ]);
-                    }
-                    throw new Error("Could not get emote sets");
-                }).catch(console.error);
-            }).then((data) => {
-                var planKeys = Object.keys(data[1]);
-                var ret = [];
-                var key;
-                for(var i = 0; i < planKeys.length; ++i) {
-                    key = planKeys[i];
-                    if(data[0].emoticon_sets.hasOwnProperty(data[1][key])) {
-                        ret.push({
-                            type: key,
-                            emotes: data[0].emoticon_sets[data[1][key]].map(function(emote) {
-                                return {
-                                    name: emote.code,
-                                    url: EmotesPanel.makeEmoteUrl(emote.id),
-                                    srcset: EmotesPanel.makeSrcSet(emote.id)
-                                };
-                            })
-                        });
-                    }
-                }
-                return ret;
-            });
-        },
-
-        getBTTVEmotes: function() {
-            return fetch('https://api.betterttv.net/2/channels/' + this.username).then(function(res) {
-                if(res.ok && res.status === 200) {
-                    return res.json();
-                }
-                throw new Error("Could not load bttv channel info");
-            }).then(function(json) {
-                return json.emotes.map((e) => {
-                    return {
-                        name: e.code,
-                        url: json.urlTemplate.replace('{{id}}', e.id).replace('{{image}}', '1x'),
-                        srcset: EmotesPanel.BTTV_SIZES.map(function(s) {
-                            return json.urlTemplate.replace("{{id}}", e.id).replace('{{image}}', s + 'x') + ' ' + s + 'x';
-                        }).join(',')
-                    };
-                });
-            });
-        },
-
-        getFFZEmotes: function() {
-            return fetch('https://api.frankerfacez.com/v1/room/id/' + this.channelId).then(function(res) {
-                if(res.ok && res.status === 200) {
-                    return res.json();
-                }
-                throw new Error("Could not load ffz channel info");
-            }).then(function(json) {
-                return json.sets[json.room.set].emoticons.map(function(emote) {
-                    return {
-                        name: emote.name,
-                        url: emote.urls["1"],
-                        srcset: Object.keys(emote.urls).map(function(s) {
-                            return emote.urls[s] + ' ' + s + 'x';
-                        }).join(', '),
-                        height: emote.height,
-                        width: emote.width
-                    };
-                });
-            })
         },
 
         setTheme: function(theme) {
